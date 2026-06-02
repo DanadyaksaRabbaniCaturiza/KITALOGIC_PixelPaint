d5bc474d46e809813d529194d0638e9f230c8419
Alvin <alvinardha3@gmail.com>
2026-06-03 00:00:24 +0700

    Commit ketiga dari NetBeans

--- src/kitalogic_pixelpaint/KITALOGIC_PixelPaint.java
+++ src/kitalogic_pixelpaint/KITALOGIC_PixelPaint.java
@@ -16,7 +16,7 @@ public class KITALOGIC_PixelPaint {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Hi");
-        System.out.print("Dunia");
+        System.out.print("Duniaku");
     }
     
 }
